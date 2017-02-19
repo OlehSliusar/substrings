c2a850f77cac612530af8c5008e997ae84cd4850
Oleh Sliusar <oleh@olehsliusar.com>
2017-02-19 10:37:41 +0200

    Remove debug lines

--- substrings.rb
+++ substrings.rb
@@ -1,10 +1,8 @@
 def substrings(words, dictionary)
-  puts "The dictionary is #{dictionary}"
   result = Hash.new(0)
   words.split.each do |word|
     dictionary.each do |dic_word|
       if word =~ /#{dic_word}/
-        puts "The word '#{word}' matched with #{dic_word}!"
         result[dic_word] += 1
       end
     end
