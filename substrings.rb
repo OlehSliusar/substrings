eb3cbd16ef32bd8cfb95b283f08549ce1b01f81c
Oleh Sliusar <oleh@olehsliusar.com>
2017-02-19 07:47:19 +0200

    Define a method

---
+++ substrings.rb
@@ -0,0 +1,8 @@
+def substrings(words, dictionary)
+
+end
+
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("below", dictionary)
