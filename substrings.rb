f28d0ca2a24bd86b87fb14266ab0e420af0a5229
Oleh Sliusar <oleh@olehsliusar.com>
2017-02-19 08:52:22 +0200

    Add bacis implementation with regex

--- substrings.rb
+++ substrings.rb
@@ -1,8 +1,15 @@
 def substrings(words, dictionary)
-
+  result = []
+  dictionary.each do |word|
+    if words =~ /#{word}/
+      puts "The word '#{word}' matched!"
+      result << word
+    end
+  end
+  result
 end
 
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-substrings("below", dictionary)
+puts substrings("below", dictionary)
